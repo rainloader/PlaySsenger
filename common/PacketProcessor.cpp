e9dab95625c69b9b044fca02ffa4aea0f787dcaf
rainloader <rainloader@gmail.com>
2014-07-24 17:46:44 +0900

    패킷 처리기 구현
    테스트 필요

--- common/PacketProcessor.cpp
+++ common/PacketProcessor.cpp
@@ -1,5 +1,8 @@
 #include "PacketProcessor.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+
 PacketProcessor::PacketProcessor()
 {
 }
@@ -19,12 +22,68 @@ bool PacketProcessor::ReadBuffer(int& idxPacket, char* packetBuffer, char* readB
 bool PacketProcessor::FetchPacket(int& protocol, char* packet, int& packetBodyLength, int& idxPacket, char* packetBuffer)
 {
 	int packetSize = 0;
+
+	if(idxPacket < sizeof(int))
+	{
+		return false;
+	}
+
+	if(memcpy(&packetSize, packetBuffer, sizeof(int)) != 0)
+	{
+		fprintf(stderr, "[ERROR] mem cpy error\n");
+		exit(-1);
+	}
+	if(packetSize >= MAX_PACKET_SIZE || packetSize <= 0)
+	{
+		fprintf(stderr, "[ERROR] invalid packet\n");
+		exit(-1);
+	}
 	
+	if(packetSize <= idxPacket)
+	{
+		memcpy(&protocol, packetBuffer + sizeof(int)/*LENGTH*/, sizeof(int));
+
+		packetBodyLength = packetSize - sizeof(int)/*LENGTH*/ - sizeof(int)/*PROTOCOL*/;
+
+		if(memcpy(packet, packetBuffer + sizeof(int)/*LENGTH*/ + sizeof(int)/*PROTOCOL*/, packetBodyLength) != 0)
+		{
+			fprintf(stderr, "[ERROR] mem cpy error\n");
+		}
+
+		if(idxPacket - packetSize > 0)
+		{
+			memmove(packetBuffer, packetBuffer + packetSize, idxPacket - packetSize);
+		}
+		
+		idxPacket -= packetSize;
+
+		if(idxPacket < 0)
+		{
+			fprintf(stderr, "[ERROR] minus packet??");
+		}
+	}
+	else
+	{
+		return false;
+	}
 	return true;
 }
 
 int PacketProcessor::WriteBuffer(const int& protocol, char* packetBuffer, char* packet, const int& packetBodyLength)
 {
-	int packetSize = 0;
+	int packetSize = sizeof(int)/*LENGTH*/ + sizeof(int)/*PROTOCOL*/ + packetBodyLength;
+	memcpy(packetBuffer, &packetSize, sizeof(int));
+	
+	if(memcpy(packetBuffer + sizeof(int)/*LENGTH*/, &protocol, sizeof(int)) != 0)
+	{
+		fprintf(stderr, "[ERROR] mem cpy error\n");
+	}
+
+	if(memcpy(packetBuffer + sizeof(int)/*LENGTH*/ + sizeof(int)/*PROTOCOL*/, packet, packetBodyLength) != 0)
+	{
+		fprintf(stderr, "[ERROR] mem cpy error\n");
+	}
+
+
 	return packetSize;
 }
