1bf331935bbb267d7447baf040647f1eccdfc2fe
rainloader <rainloader@gmail.com>
2014-07-28 17:22:33 +0900

    NetworkManager 모듈 구현중

--- client/NetworkManager.cpp
+++ client/NetworkManager.cpp
@@ -1,33 +1,61 @@
 #include "NetworkManager.h"
 #include "../common/Structures.h"
+#include "../common/PacketProcessor.h"
+#include "../common/Packets.h"
 
 #include <strings.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 
 #define SERVER_ADDRESS "127.0.0.1"
 
 void NetworkManager::Initialize()
+{        
+	if((socketFd = socket(PF_INET, SOCK_STREAM, 0))<0)
+        {
+                fprintf(stderr, "[ERROR] : SOCKET ERROR\n");
+                exit(-1);
+        }
+	idxPacket = 0;
+}
+
+bool NetworkManager::Connect()
 {
 	struct sockaddr_in serverAddr;
         bzero((char *) &serverAddr, sizeof(serverAddr));
         serverAddr.sin_family = PF_INET;
         serverAddr.sin_addr.s_addr = inet_addr(SERVER_ADDRESS);
         serverAddr.sin_port = htons(PORT_NUM);
-/*
-        if((clientSocket = socket(PF_INET, SOCK_STREAM, 0))<0)
-        {
-                fprintf(stderr, "[ERROR] : SOCKET ERROR\n");
-                return -1;
-        }*/
-}
 
-void NetworkManager::Connect()
-{
+	if(connect(socketFd, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0)
+	{
+		fprintf(stderr, "[ERROR] : CONNECT ERROR\n");
+		return false;
+	}
+	printf("Connected");
+	return true;
 }
 
 void NetworkManager::ReadAndDispatch()
 {
+	int result = 0;
+	result = read(socketFd, readBuffer, MAX_BUFFER_LENGTH);
+
+	PacketProcessor::ReadBuffer(idxPacket, packetBuffer, readBuffer, result);
+
+	int protocol;
+	Packet packet;
+	int packetBodyLength;
+	while(PacketProcessor::FetchPacket(protocol, packet.buffer, packetBodyLength, idxPacket, packetBuffer))
+	{
+		if(0 <= protocol && protocol < PT_MAX)
+		{
+			
+		}
+	}
 }
 
--- client/NetworkManager.h
+++ client/NetworkManager.h
@@ -1,18 +1,21 @@
 #ifndef _NETWORKMANAGER_H
 #define _NETWORKMANAGER_H
 
+#include "../common/Structures.h"
 
 class NetworkManager
 {
 public:
 	void Initialize();
-	void Connect();
+	bool Connect();
 	void ReadAndDispatch();
 
 private:
 	int sessionId;
 	int socketFd;
-	
+	char packetBuffer[MAX_PACKET_SIZE];
+	char readBuffer[MAX_BUFFER_LENGTH];
+	int idxPacket;
 };
 
 #endif
